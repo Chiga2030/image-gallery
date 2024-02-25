9676c76be6e6dea865e613537bf8f4cb6552a973
Anton Tkachuk <tkachuk.ant@mail.ru>
2024-02-25 20:51:56 +0300

    created loader component

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import styles from './Loader.module.css'
+
+
+export const Loader = (): JSX.Element => {
+  return (
+    <div className={styles['loadingio-eclipse']}>
+      <div className={styles['ldio-rpinwye8j0b']}>
+        <div/>
+      </div>
+    </div>
+  )
+}
